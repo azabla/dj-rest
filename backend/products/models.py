98c46d4790be02b3b43dc92d19a15082caee3242
azabla <andualemchanie.y@gmail.com>
2025-10-24 08:46:32 +0300

    listcreateapiview and listapiview

--- backend/products/models.py
+++ backend/products/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=15)
+    title = models.CharField(max_length=30)
     content = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=15, decimal_places=2)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.00)
 
     @property
     def sale_price(self):
